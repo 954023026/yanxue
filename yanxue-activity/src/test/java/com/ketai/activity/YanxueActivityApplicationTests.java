7d474d94b9224a0522a03a4437d5c98dda679657
admin <zxb0820@126.com>
2020-01-06 16:23:59 +0800

    注册中心与网关项目搭建完成。

--- yanxue-activity/src/test/java/com/ketai/activity/YanxueActivityApplicationTests.java
+++ yanxue-activity/src/test/java/com/ketai/activity/YanxueActivityApplicationTests.java
@@ -1,13 +1,8 @@
 package com.ketai.activity;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class YanxueActivityApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
 
 }
